eada768437433e23c6e4c56f6a3f3cc9d7e7d38e
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-05-04 21:26:28 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_lambda_function" "default" {
   }
 
   vpc_config {
-    security_group_ids = length(var.subnet_ids) > 0 ? concat[var.existing_security_groups, aws_security_group.default.*.id]
+    security_group_ids = length(var.subnet_ids) > 0 ? concat[var.existing_security_groups, aws_security_group.default.*.id] : []
     subnet_ids         = length(var.subnet_ids) > 0 ? var.subnet_ids : []
   }
 }
